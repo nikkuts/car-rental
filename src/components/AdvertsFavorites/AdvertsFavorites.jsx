997d3e1498301ab528a1329c8538c58b2b1cf508
nikkuts <nickkuts888@gmail.com>
2023-09-20 00:54:31 +0300

    updated Favorites

--- src/components/AdvertsFavorites/AdvertsFavorites.jsx
+++ src/components/AdvertsFavorites/AdvertsFavorites.jsx
@@ -30,7 +30,7 @@ export const AdvertsFavorites = () => {
 
     useEffect(() => {
         handleFetchAdverts();
-    },[switcher, adverts]);
+    },[switcher]);
 
     return (
         isRender &&
