16079b0a90bfc04c02fcf9aad93bfe3b93d119c0
nikkuts <nickkuts888@gmail.com>
2023-09-21 00:25:03 +0300

    updated Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,22 @@
+const styles = {
+    container: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    title: {
+      fontWeight: 500,
+      fontSize: 48,
+      textAlign: 'center',
+    },
+  };
+
 export const Home = () => {
     return (
-        <>
-        Home
-        </>
+        <div style={styles.container}>
+        <h1 style={styles.title}>
+        Application for choosing cars for rent in Ukraine{' '}
+        </h1>
+      </div>
     )
 };
